6c7ae56d9733f10f2ec67e37eaeee1052860c1e5
Marcelo Sant'Anna <marcelojunin2010@hotmail.com>
2023-05-01 14:06:39 -0300

    Retrieving products and shows in console

--- src/app/home/list/list.component.ts
+++ src/app/home/list/list.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
 export class ListComponent implements OnInit {
-
-  constructor() { }
+  constructor(private productService: ProductService) {}
 
   ngOnInit() {
-
+    this.productService.getAll().subscribe(console.log);
   }
 }
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../models/product';
+import { environment as env } from 'src/environment/environment';
+import { Paths } from '../constants/paths';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  constructor(private http: HttpClient) {}
+
+  public getAll(): Observable<Product[]> {
+    return this.http.get<Product[]>(`${env.apiUrl}${Paths.PRODUCTS}`);
+  }
+}
